621762e7a2fb5aeaaa453f0503ea1e1e9c650552
Dawood Trumboo <107537057+dawoodtrumboo@users.noreply.github.com>
2023-09-27 21:20:01 +0530

    new Updates

--- src/components/DetailBar.jsx
+++ src/components/DetailBar.jsx
@@ -4,7 +4,7 @@ import {quizData} from '../constant/quizData'
 
 const DetailBar = () => {
   return (
-    <div className='flex flex-col items-center py-4  bg-white w-[20%] overflow-y-scroll max-h-[800px]'>
+    <div className='flex flex-col items-center py-4  bg-white w-[20%] max-h-[800px]'>
      <div className='py-5 px-2 flex flex-col items-center bg-gradient-to-b from-[#8C6FE9] to-[#2D00BA] text-white text-3xl font-bold w-[80%] rounded-lg'>
         <p className='text-xs font-normal'>Total Quizes</p>
         <h1>{quizData.length}</h1>
@@ -19,7 +19,7 @@ const DetailBar = () => {
         option={quiz.options}
         />
       ))} */}
-        <div className='w-full'>
+        <div className='w-full max-h-[600px] overflow-y-scroll'>
       {quizData.map((quiz, index) => (
         <QuizCard
           key={index}
